7a43cca27c65ab6cb3befc15b7ce25b3b3a7586b
Srikant Sahoo <srikantsahoo3601@gmail.com>
2019-10-07 20:24:55 +0530

    Modifying the full code but its not correct
    develop it more. aur bhi Anygrid logic mein middle edge  elements ka logic missing hai

--- app/src/main/java/com/graspit/tictactoe/AnyGridLogicTTT.cpp
+++ app/src/main/java/com/graspit/tictactoe/AnyGridLogicTTT.cpp
@@ -7,274 +7,308 @@ int index=0;
 
 int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};      //2=unplayed  //total 25
 
-void edgeLeftToRight1(int row, int col){
+int edgeLeftToRight1(int row, int col){
 
 	index = (row*gridCol)+col;
 	if((gameState[index]==0) && (gameState[index+1]==0) && (gameState[index+2]==0))
 		//mark the 0 boxes
 		{
-			;
+			return 0;//grid2(index+1 +/- something).setImageResource(green);		//etc
 		}
 	else if((gameState[index]==1) && (gameState[index+1]==1) && (gameState[index+2]==1))
 		//mark the 1 boxes
 		{
-			;
+			return 1;
 		}
 	//write the codes for points and time to appropriate player
+			return 2;
 }
-void edgeRightToleft2(int row, int col){
+int edgeRightToleft2(int row, int col){
 	
 	index = (row*gridCol)+col;
 	if((gameState[index]==0) && (gameState[index-1]==0) && (gameState[index-2]==0))
-		;//mark the 0 boxes
+		return 0;//mark the 0 boxes
 	else if((gameState[index]==1) && (gameState[index-1]==1) && (gameState[index-2]==1))
-		;//mark the 1 boxes
+		return 1;//mark the 1 boxes
 
 	//write the codes for points and time to appropriate player
+		return 2;
 }
-void edgeTopToBottom3(int row, int col){
+int edgeTopToBottom3(int row, int col){
 
 	index = (row*gridCol)+col;
 	if((gameState[index]==0) && (gameState[index+gridCol]==0) && (gameState[index+(gridCol*2)]==0))
-		;//mark the 0 boxes
+		return 0;//mark the 0 boxes
 	else if((gameState[index]==1) && (gameState[index+gridCol]==1) && (gameState[index+(gridCol*2)]==1))
-		;//mark the 1 boxes
+		return 1;//mark the 1 boxes
 
 	//write the codes for points and time to appropriate player
+		return 2;
 }
-void edgeBottomToTop4(int row, int col){
+int edgeBottomToTop4(int row, int col){
 
 	index = (row*gridCol)+col;
 	if((gameState[index]==0) && (gameState[index-gridCol]==0) && (gameState[index-(gridCol*2)]==0))
-		;//mark the 0 boxes
+		return 0;//mark the 0 boxes
 	else if((gameState[index]==1) && (gameState[index-gridCol]==1) && (gameState[index-(gridCol*2)]==1))
-		;//mark the 1 boxes
+		return 1;//mark the 1 boxes
 
 	//write the codes for points and time to appropriate player	
+		return 2;
 }
-void edgeDiagBLeftToTRight5(int row, int col){
+int edgeDiagBLeftToTRight5(int row, int col){
 
 	index = (row*gridCol)+col;
 	if((gameState[index]==0) && (gameState[index-gridCol-1]==0) && (gameState[index-((gridCol-1)*2)]==0))
-		;//mark the 0 boxes
+		return 0;//mark the 0 boxes
 	else if((gameState[index]==1) && (gameState[index-gridCol-1]==1) && (gameState[index-((gridCol-1)*2)]==1))
-		;//mark the 1 boxes
+		return 1;//mark the 1 boxes
 
-	//write the codes for points and time to appropriate player		
+	//write the codes for points and time to appropriate player	
+		return 2;	
 }
-void edgeDiagTRightToBLeft6(int row, int col){
+int edgeDiagTRightToBLeft6(int row, int col){
 
 	index = (row*gridCol)+col;
 	if((gameState[index]==0) && (gameState[index+gridCol-1]==0) && (gameState[index+((gridCol-1)*2)]==0))
-		;//mark the 0 boxes
+		return 0;//mark the 0 boxes
 	else if((gameState[index]==1) && (gameState[index+gridCol-1]==1) && (gameState[index+((gridCol-1)*2)]==1))
-		;//mark the 1 boxes
+		return 1;//mark the 1 boxes
 
 	//write the codes for points and time to appropriate player	
+		return 2;
 }
-void edgeDiagBRightToTLeft7(int row, int col){
+int edgeDiagBRightToTLeft7(int row, int col){
 
 	index = (row*gridCol)+col;
 	if((gameState[index]==0) && (gameState[index-(gridCol+1)]==0) && (gameState[index-((gridCol+1)*2)]==0))
-		;//mark the 0 boxes
+		return 0;//mark the 0 boxes
 	else if((gameState[index]==1) && (gameState[index-(gridCol+1)]==1) && (gameState[index-((gridCol+1)*2)]==1))
-		;//mark the 1 boxes
+		return 1;//mark the 1 boxes
 
 	//write the codes for points and time to appropriate player
+		return 2;
 }
-void edgeDiagTLeftToBRight8(int row, int col){
+int edgeDiagTLeftToBRight8(int row, int col){
 
 	index = (row*gridCol)+col;
 	if((gameState[index]==0) && (gameState[index+gridCol+1]==0) && (gameState[index+((gridCol+1)*2)]==0))
-		;//mark the 0 boxes
+		return 0;//mark the 0 boxes
 	else if((gameState[index]==1) && (gameState[index+gridCol+1]==1) && (gameState[index+((gridCol+1)*2)]==1))
-		;//mark the 1 boxes
+		return 1;//mark the 1 boxes
 
-	//write the codes for points and time to appropriate player		
+	//write the codes for points and time to appropriate player	
+		return 2;	
 }
-void midTopBottom9(int row, int col){
+int midTopBottom9(int row, int col){
 
 	index = (row*gridCol)+col;
 	if((gameState[index]==0) && (gameState[index-gridCol]==0) && (gameState[index+gridCol]==0))
-		;//mark the 0 boxes
+		return 0;//mark the 0 boxes
 	else if((gameState[index]==1) && (gameState[index-gridCol]==1) && (gameState[index+gridCol]==1))
-		;//mark the 1 boxes
+		return 1;//mark the 1 boxes
 
 	//write the codes for points and time to appropriate player			
+	return 2;	
 }
-void midLeftRight10(int row, int col){
+int midLeftRight10(int row, int col){
 
 	index = (row*gridCol)+col;
 	if((gameState[index]==0) && (gameState[index+1]==0) && (gameState[index-1]==0))
-		;//mark the 0 boxes
+		return 0;//mark the 0 boxes
 	else if((gameState[index]==1) && (gameState[index+1]==1) && (gameState[index-1]==1))
-		;//mark the 1 boxes
+		return 1;//mark the 1 boxes
 
-	//write the codes for points and time to appropriate player	
+	//write the codes for points and time to appropriate player		
+	return 2;
 }
-void midDiagII11 (int row, int col){
+int midDiagII11 (int row, int col){
 
 	index = (row*gridCol)+col;
 	if((gameState[index]==0) && (gameState[index+(gridCol+1)]==0) && (gameState[index-(gridCol+1)]==0))
-		;//mark the 0 boxes
+		return 0;//mark the 0 boxes
 	else if((gameState[index]==1) && (gameState[index+(gridCol+1)]==1) && (gameState[index-(gridCol+1)]==1))
-		;//mark the 1 boxes
+		return 1;//mark the 1 boxes
 
-	//write the codes for points and time to appropriate player	
+	//write the codes for points and time to appropriate player		
+	return 2;
 }
-void midDiag12(int row, int col){
+int midDiag12(int row, int col){
 
 	index = (row*gridCol)+col;
 	if((gameState[index]==0) && (gameState[index+gridCol-1]==0) && (gameState[index-(gridCol-1)]==0))
-		;//mark the 0 boxes
+		return 0;//mark the 0 boxes
 	else if((gameState[index]==1) && (gameState[index+gridCol-1]==1) && (gameState[index-(gridCol-1)]==1))
-		;//mark the 1 boxes
+		return 1;//mark the 1 boxes
 
 	//write the codes for points and time to appropriate player		
+	return 2;
 }
 
 
-void callingStructure(){
-
+int wincheck(int row, int col){
+    int temp=2;
 	//middle elements
 	if((row != 0)||(row != (n-1))||(col != 0) || (col != (n-1)))
 	{
-		edgeLeftToRight1(row , col);
-		edgeRightToleft2(row , col);
-		edgeTopToBottom3(row , col);
-		edgeBottomToTop4(row , col);
-		edgeDiagBLeftToTRight5(row , col);
-		edgeDiagTRightToBLeft6(row , col);
-		edgeDiagBRightToTLeft7(row , col);
-		edgeDiagTLeftToBRight8(row , col);
-		midTopBottom9(row , col);
-		midLeftRight10(row , col);
-		midDiagII11(row , col);
-		midDiag12(row , col);
+		temp = edgeLeftToRight1(row , col);
+		if(temp==2)
+		temp = edgeRightToleft2(row , col);
+		else if(temp==2)
+		temp = edgeTopToBottom3(row , col);
+		else if(temp==2)
+		temp = edgeBottomToTop4(row , col);
+		else if(temp==2)
+		temp = edgeDiagBLeftToTRight5(row , col);
+		else if(temp==2)
+		temp = edgeDiagTRightToBLeft6(row , col);
+		else if(temp==2)
+		temp = edgeDiagBRightToTLeft7(row , col);
+		else if(temp==2)
+		temp = edgeDiagTLeftToBRight8(row , col);
+		else if(temp==2)
+		temp = midTopBottom9(row , col);
+		else if(temp==2)
+		temp = midLeftRight10(row , col);
+		else if(temp==2)
+		temp = midDiagII11(row , col);
+		else if(temp==2)
+		temp = midDiag12(row , col);
+		else;
+		return (temp);
 	}
 	//edge elements
 	else{
 	//left element
 		if(col == 0)	
 		{
-			edgeLeftToRight1(row,col);
+			temp = edgeLeftToRight1(row,col);
 			if(row == 0 || row == 1)			//top Left
-			{
-				edgeDiagTLeftToBRight8(row,col);
-				edgeTopToBottom3(row,col);
+			{	
+				if(temp==2)
+				temp = edgeDiagTLeftToBRight8(row,col);
+				else if(temp==2)
+				temp = edgeTopToBottom3(row,col);
+				else;
+
 				if(row == 1)
-					midTopBottom9(row,col);	
+					if(temp==2)
+					temp = midTopBottom9(row,col);	
+				return (temp);
 			}
 
 			else if((row == (n-1)) || (row == (n-2))) 			//bottom left
 			{
-				edgeBottomToTop4(row,col);
-				edgeDiagBLeftToTRight5(row,col);
+				temp = edgeBottomToTop4(row,col);
+				temp = edgeDiagBLeftToTRight5(row,col);
 				if(row == (n-2))
-					midTopBottom9(row,col);
+					temp = midTopBottom9(row,col);
+				return (temp);
 			}
 			else			//middle left
 			{
-				edgeBottomToTop4(row , col);
-				edgeTopToBottom3(row , col);
-				midTopBottom9(row , col);
-				edgeDiagTLeftToBRight8(row , col);
-				edgeDiagBLeftToTRight5(row , col);
+				temp = edgeBottomToTop4(row , col);
+				temp = edgeTopToBottom3(row , col);
+				temp = midTopBottom9(row , col);
+				temp = edgeDiagTLeftToBRight8(row , col);
+				temp = edgeDiagBLeftToTRight5(row , col);
+				return (temp);
 			}
 		}
 	//Right Element
 		else if(col == (n-1))
 		{
-			edgeRightToleft2();
+			temp = edgeRightToleft2();
 			if(row == 0 || row == 1)			//top right
 			{
-				edgeTopToBottom3(row,col);
-				edgeDiagTRightToBLeft6(row,col);
+				temp = edgeTopToBottom3(row,col);
+				temp = edgeDiagTRightToBLeft6(row,col);
 				if(row == 1)
-					midTopBottom9(row,col);	
+					temp = midTopBottom9(row,col);	
+				return (temp);
 			}
 
 			else if((row == (n-1)) || (row == (n-2))) 			//bottom right
 			{
-				edgeBottomToTop4(row,col);
-				edgeDiagBRightToTLeft7(row,col);
+				temp = edgeBottomToTop4(row,col);
+				temp = edgeDiagBRightToTLeft7(row,col);
 				if(row == (n-2))
-					midTopBottom9(row,col);
+					temp = midTopBottom9(row,col);
+				return (temp);
 			}
 			else			//middle right
 			{
-				edgeBottomToTop4(row , col);
-				edgeTopToBottom3(row , col);
-				midTopBottom9(row , col);
-				edgeDiagBRightToTLeft7(row , col);
-				edgeDiagTRightToBLeft6(row , col);
+				temp = edgeBottomToTop4(row , col);
+				temp = edgeTopToBottom3(row , col);
+				temp = midTopBottom9(row , col);
+				temp = edgeDiagBRightToTLeft7(row , col);
+				temp = edgeDiagTRightToBLeft6(row , col);
+				return (temp);
 			}
 		}
 	//Bottom element
 		else if(row == (n-1))
 		{
-			edgeBottomToTop4(row,col);
+			temp = edgeBottomToTop4(row,col);
 			if(col == 0 || col == 1)			//bottom left
 			{
-				edgeLeftToRight1(row,col);
-				edgeDiagBLeftToTRight5(row,col);
+				temp = edgeLeftToRight1(row,col);
+				temp = edgeDiagBLeftToTRight5(row,col);
 				if(col == 1)
-					midLeftRight10(row,col);	
+					temp = midLeftRight10(row,col);	
+				return (temp);
 			}
 
 			else if((col == (n-1)) || (col == (n-2))) 			//bottom right
 			{
-				edgeRightToleft2(row,col);
-				edgeDiagBRightToTLeft7(row,col);
+				temp = edgeRightToleft2(row,col);
+				temp = edgeDiagBRightToTLeft7(row,col);
 				if(col == (n-2))
-					midLeftRight10(row,col);
+					temp = midLeftRight10(row,col);
+				return (temp);
 			}
 			else			//middle bottom
 			{
-				edgeLeftToRight1(row , col);
-				edgeRightToleft2(row , col);
-				midLeftRight10(row , col);
-				edgeDiagBLeftToTRight5(row , col);
-				edgeDiagBRightToTLeft7(row , col);
+				temp = edgeLeftToRight1(row , col);
+				temp = edgeRightToleft2(row , col);
+				temp = midLeftRight10(row , col);
+				temp = edgeDiagBLeftToTRight5(row , col);
+				temp = edgeDiagBRightToTLeft7(row , col);
+				return (temp);
 			}		
 		}
 
 	//Top element
 		else if(row == 0)
 		{
-			edgeTopToBottom3(row,col);
+			temp = edgeTopToBottom3(row,col);
 			if(col == 0 || col == 1)			//top left
 			{
-				edgeLeftToRight1(row,col);
-				edgeDiagTLeftToBRight8(row,col);
+				temp = edgeLeftToRight1(row,col);
+				temp = edgeDiagTLeftToBRight8(row,col);
 				if(col == 1)
-					midLeftRight10(row,col);	
+					temp = midLeftRight10(row,col);
+
+				return (temp);	
 			}
 
 			else if((col == (n-1)) || (col == (n-2))) 			//top right
 			{
-				edgeRightToleft2(row,col);
-				edgeDiagTRightToBLeft6(row,col);
+				temp = edgeRightToleft2(row,col);
+				temp = edgeDiagTRightToBLeft6(row,col);
 				if(col == (n-2))
-					midLeftRight10(row,col);
+					temp = midLeftRight10(row,col);
+				return (temp);
 			}
 			else			//middle top
 			{
-				edgeLeftToRight1(row , col);
-				edgeRightToleft2(row , col);
-				midLeftRight10(row , col);
-				edgeDiagTLeftToBRight8(row , col);
-				edgeDiagTRightToBLeft6(row , col);
+				temp = edgeLeftToRight1(row , col);
+				temp = edgeRightToleft2(row , col);
+				temp = midLeftRight10(row , col);
+				temp = edgeDiagTLeftToBRight8(row , col);
+				temp = edgeDiagTRightToBLeft6(row , col);
+				return (temp);
 			}	
 		}
 	}
-}
-
-
-
-
-int main(){
-
-
-	return 0;
 }
--- app/src/main/java/com/graspit/tictactoe/GamePlay.java
+++ app/src/main/java/com/graspit/tictactoe/GamePlay.java
@@ -8,6 +8,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+/*
 public class GamePlay extends AppCompatActivity {
 
         int activePlayer = 0;   //0=cross    1=circle
@@ -47,7 +48,8 @@ public class GamePlay extends AppCompatActivity {
                 {
                     if(gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
                             gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
-                            gameState[winningPosition[0]]!=2){
+                            gameState[winningPosition[0]]!=2)
+                    {
 
                         gameIsActive= false;
                         String winner="Player Two (Taken Cricle)";
@@ -65,7 +67,8 @@ public class GamePlay extends AppCompatActivity {
 
                         layout.setVisibility(View.VISIBLE);
                     }
-                    else{
+                    else
+                    {
                         boolean gameIsOver = true;
                         for(int counterState : gameState) {
                             if (counterState == 2)
@@ -82,9 +85,9 @@ public class GamePlay extends AppCompatActivity {
                             layout.setVisibility(View.VISIBLE);
                         }
                     }
-                }
-            }
-        }
+                }	//end of for loop
+            }	//end of if condition
+        }	//end of Gameplay
 
         public void playAgain(View view){
 
@@ -102,7 +105,6 @@ public class GamePlay extends AppCompatActivity {
 
             for(int i=0; i<gridLayout.getChildCount(); i++)
                 ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
-
         }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -120,4 +122,167 @@ public class GamePlay extends AppCompatActivity {
         });
     }
 
+}*/
+
+//Modification
+
+//Declare the timer 
+ new CountDownTimer5(5000, 1000) {
+      public void onTick(long millisUntilFinished) {
+          mTextField.setText("seconds remaining\n" + millisUntilFinished / 1000);
+      }
+
+      public void onFinish() {
+          mTextField.setText("done");
+      }  
+}.start();
+
+
+ new CountDownTimer10(5000, 1000) {
+      public void onTick(long millisUntilFinished) {
+          mTextField.setText("seconds remaining\n" + millisUntilFinished / 1000);
+      }
+
+      public void onFinish() {
+          mTextField.setText("done");
+      }  
+}.start();
+
+////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class GamePlay extends AppCompatActivity {
+
+        int activePlayer = 0;   //0=cross    1=circle
+        boolean gameIsActive = true;
+        int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};      //2=unplayed
+        //int[][] winningPosition = {{0,1,2} , {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
+        int totalScore0=0, totalScore1=0, temphaswon;
+
+gridLayout.onClick()
+{ ImageView counter = (ImageView) view;
+
+    int tappedCounter =  Integer.parseInt(counter.getTag().toString());	//getTag to getrow and getcol
+    /*
+	int tappedEleRow =  Integer.parseInt(counter.getRow().toString());
+	int tappedEleCol =  Integer.parseInt(counter.getCol().toString());
+	*/
+
+	timerpoint = timerPoints();		//after pausing the time return and 
+	temphaswon = wincheck(tappedEleRow, tappedEleCol);	//i.e player 1 or 0
+
+
+    System.out.println(counter.getTag().toString());
+    TextView playerTurn =findViewById(R.id.playersTurn);
+
+    if(gameState[tappedEleRow*(gridCol)+tappedEleCol]==2 && gameIsActive) {
+
+        counter.setTranslationY(-1000f);
+        gameState[tappedEleRow*(gridCol)+tappedEleCol]=activePlayer;
+        if (activePlayer == 0) {
+        	if(temphaswon != -1)
+        		totalScore0 += timerpoint + 1000;		//or somthing else
+            counter.setImageResource(R.drawable.group_7);
+            activePlayer = 1;
+            playerTurn.setText(PlayerName + " Two Turn");		//player1name to the stored arraylist
+        }
+        else {
+        	if(temphaswon != -1)
+        		totalScore1 += timerpoint + 1000;		//or somthing else
+            counter.setImageResource(R.drawable.ellipse_2);
+            gameState[tappedEleRow*(gridCol)+tappedEleCol] = 1;	//I think this statement is redundant
+            playerTurn.setText("Player One Turn");
+            activePlayer = 0;
+        }
+        counter.animate().translationYBy(1000f).setDuration(300);
+
+            	boolean gameIsOver = true;
+                for(int counterState : gameState) {
+                    if (counterState == 2)
+                        gameIsOver = false;
+                }
+          		
+                if(gameIsOver){
+                    gameIsActive= false;
+                    String winner="Player Two (Taken Cricle)";
+                    if(totalScore0>totalScore1){
+                        winner = "Player One (Taken Cross)";
+                    }
+
+                    System.out.println("Player " + haswon + " has won the game");
+                    TextView winnerMessage = findViewById(R.id.winnertext);			//Add some more things
+                    winnerMessage.setText(winner + " has won");						//showResult() saveResult() updateLearderBoard()
+                    LinearLayout layout = findViewById(R.id.playAgainLinearLayout);
+                    layout.setVisibility(View.VISIBLE);
+                }
+                else if(totalScore0=totalScore1){
+					//Add some more things
+                    TextView winnerMessage = findViewById(R.id.winnertext);		//showResult() saveResult() updateLearderBoard()
+                    winnerMessage.setText("It's a draw");
+                    LinearLayout layout = findViewById(R.id.playAgainLinearLayout);
+                    layout.setVisibility(View.VISIBLE);
+                }
+	}
 }
+
+
+        public void playAgain(View view){
+
+            gameIsActive= true;
+            LinearLayout layout = findViewById(R.id.playAgainLinearLayout);
+
+            layout.setVisibility(View.INVISIBLE);
+
+            activePlayer = 0;   //0=cross    1=circle
+
+            for(int i=0; i<gameState.length; i++)
+                gameState[i]=2;
+
+            android.support.v7.widget.GridLayout gridLayout = findViewById(R.id.gridLayout);
+
+            for(int i=0; i<gridLayout.getChildCount(); i++)
+                ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_play);
+
+        TextView playersTurnText = findViewById(R.id.playersTurn);
+        playersTurnText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homescreen = new Intent(GamePlay.this,SettingsPanel.class);
+                startActivity(homescreen);
+                finish();
+            }
+        });
+                //All intents during gameplay
+    }
+
+
+/////
+start the timer and pause the timer when the player clicks the view.
+
+public int timerPoints(){
+	gridLayout.onClick(){
+		pause timer;
+		int timerpoints = millisecond left * 0.8;
+		return timerpoints;
+}
+
+
